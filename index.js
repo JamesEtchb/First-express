b00de57d3b7741299b6177a42420fcd11cef64a2
James Etchberger <jamesetchberger@Jamess-MacBook-Air.local>
2022-07-26 09:29:12 -0400

    prettied it up a bit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //imoport express library
-import express from "express"
+import express from 'express'
 
 //initialize express server
 const app = express()
@@ -7,51 +7,43 @@ const app = express()
 //telling express to expect things getting pused to be json
 app.use(express.json())
 
-
 //add new student (in theory)
 app.post('/students', (request, response) => {
-    const newStudent = request.body
-    students.push(newStudent)
-    response.status(201).send(students)
+  const newStudent = request.body
+  students.push(newStudent)
+  response.status(201).send(students)
 })
-
 
 // list requests that we want express to respond too
 // ROUTES ALWAYS NEED A / AT THE BEGINNING
-app.get("/hello", (request, response) => {
+app.get('/hello', (request, response) => {
   //do something when we get a request to this route
-  response.send("Hello World!")
+  response.send('Hello World!')
 })
 
-
 const students = [
-  { firstName: "Jonathan", lastName: "Vegas" },
-  { firstName: "Bridgette", lastName: "Lemus" },
-  { firstName: "Mason", lastName: "Madaras" },
-  { firstName: "Logan", lastName: "McCalley" },
-  { firstName: "James", lastName: "Etchberger" },
+  { firstName: 'Jonathan', lastName: 'Vegas' },
+  { firstName: 'Bridgette', lastName: 'Lemus' },
+  { firstName: 'Mason', lastName: 'Madaras' },
+  { firstName: 'Logan', lastName: 'McCalley' },
+  { firstName: 'James', lastName: 'Etchberger' },
 ]
 
-
-app.get("/students", (request, response) => {
+app.get('/students', (request, response) => {
   response.send(students)
 })
 
 //this will return the first value from the array that equals true
-app.get("/students/:fName", (request, response) => {
+app.get('/students/:fName', (request, response) => {
   const student = students.find(
     (stud) => stud.firstName === request.params.fName
   )
   if (!student) {
-    response
-      .status(404)
-      .send({ message: "Student not found", success: false })
+    response.status(404).send({ message: 'Student not found', success: false })
     return
   }
   response.send(student)
 })
-
-
 
 //start listening on a port
 //this must always go at the END
